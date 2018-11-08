a756457dd436a30532c7d1621c86036de7b1a1d0
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2018-11-08 08:43:37 +0100

    added more examples

--- src/ConsoleAppClassic/MainTest.cs
+++ src/ConsoleAppClassic/MainTest.cs
@@ -9,35 +9,51 @@ namespace ConsoleAppClassic
     {
         public static void Run()
         {
-            var randomizerTextPattern = RandomizerFactory.GetRandomizer(new FieldOptionsTextPattern { Pattern = "\\xLLnn_**ss\\x" });
-            string textPattern = randomizerTextPattern.Generate();
-            Write(randomizerTextPattern, textPattern);
+            var randomizerCity = RandomizerFactory.GetRandomizer(new FieldOptionsCity());
+            string city = randomizerCity.Generate();
+            Write(randomizerCity, city);
+
+            var randomizerCountry = RandomizerFactory.GetRandomizer(new FieldOptionsCountry());
+            string country = randomizerCountry.Generate();
+            Write(randomizerCountry, country);
+
+            var randomizerDateTime = RandomizerFactory.GetRandomizer(new FieldOptionsDateTime { DateFrom = DateTime.Today.AddDays(-1888) });
+            DateTime? date = randomizerDateTime.Generate();
+            Write(randomizerDateTime, date);
+
+            var randomizerFirstName = RandomizerFactory.GetRandomizer(new FieldOptionsFirstName());
+            string firstname = randomizerFirstName.Generate();
+            Write(randomizerFirstName, firstname);
+
+            var randomizerInteger = RandomizerFactory.GetRandomizer(new FieldOptionsInteger());
+            int? integer = randomizerInteger.Generate();
+            Write(randomizerInteger, integer);
+
+            var randomizerGuid = RandomizerFactory.GetRandomizer(new FieldOptionsGuid());
+            Guid? guid = randomizerGuid.Generate();
+            Write(randomizerGuid, guid);
+            string guidAsString = randomizerGuid.GenerateAsString();
+            Write(randomizerGuid, guidAsString);
+
+            var randomizerStringList = RandomizerFactory.GetRandomizer(new FieldOptionsStringList { Values = new[] { "1", "b" }.ToList() });
+            string stringListValue = randomizerStringList.Generate();
+            Write(randomizerStringList, stringListValue);
 
             var randomizerText = RandomizerFactory.GetRandomizer(new FieldOptionsText { UseNumber = false, UseSpecial = false });
             string text = randomizerText.Generate();
             Write(randomizerText, text);
 
-            var randomizerFirstName = RandomizerFactory.GetRandomizer(new FieldOptionsFirstName());
-            Write(randomizerFirstName, randomizerFirstName.Generate());
+            var randomizerTextLipsum = RandomizerFactory.GetRandomizer(new FieldOptionsTextLipsum { Paragraphs = 1 });
+            string lipsum = randomizerTextLipsum.Generate();
+            Write(randomizerTextLipsum, lipsum);
 
-            var randomizerGuid = RandomizerFactory.GetRandomizer(new FieldOptionsGuid());
-            Write(randomizerGuid, randomizerGuid.Generate());
-            Write(randomizerGuid, randomizerGuid.GenerateAsString());
-
-            var randomizerInteger = RandomizerFactory.GetRandomizer(new FieldOptionsInteger());
-            Write(randomizerInteger, randomizerInteger.Generate());
-
-            var randomizerDateTime = RandomizerFactory.GetRandomizer(new FieldOptionsDateTime { DateFrom = DateTime.Today.AddDays(-1888) });
-            Write(randomizerDateTime, randomizerDateTime.Generate());
-
-            var randomizerStringList = RandomizerFactory.GetRandomizer(new FieldOptionsStringList { Values = new[] { "1", "b" }.ToList() });
-            Write(randomizerStringList, randomizerStringList.Generate());
+            var randomizerTextPattern = RandomizerFactory.GetRandomizer(new FieldOptionsTextPattern { Pattern = "\\xLLnn_**ss\\x" });
+            string textPattern = randomizerTextPattern.Generate();
+            Write(randomizerTextPattern, textPattern);
 
             var randomizerTextWords = RandomizerFactory.GetRandomizer(new FieldOptionsTextWords { Min = 10, Max = 10 });
-            Write(randomizerTextWords, randomizerTextWords.Generate());
-
-            var randomizerTextLipsum = RandomizerFactory.GetRandomizer(new FieldOptionsTextLipsum { Paragraphs = 1 });
-            Write(randomizerTextLipsum, randomizerTextLipsum.Generate());
+            string words = randomizerTextWords.Generate();
+            Write(randomizerTextWords, words);
         }
 
         private static void Write<T>(object randomizer, T value)
