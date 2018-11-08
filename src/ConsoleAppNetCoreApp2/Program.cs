60a14f11255409f85979109b14305f807069762e
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2018-11-08 08:36:45 +0100

    ConsoleAppNetCoreApp2

---
+++ src/ConsoleAppNetCoreApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleAppClassic;
+
+namespace ConsoleAppNetCoreApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainTest.Run();
+        }
+    }
+}
